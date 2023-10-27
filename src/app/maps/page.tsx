31592294b474f2d67171a4e50cd060324b54221e
Heitcleiff <89062666+Heitclieff@users.noreply.github.com>
2023-10-27 19:26:31 +0700

    Updated Projects structure

---
+++ src/app/maps/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MapPage from '@/features/map'
+import {Wrapper} from '@googlemaps/react-wrapper'
+
+
+export default function Maps() {
+  return (
+      <MapPage/>
+  )
+}
